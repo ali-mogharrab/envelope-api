e481fc21465009bc1fcedb11e62432976c14536e
Ali Mogharrab <alimogharrab11@gmail.com>
2023-08-11 20:05:02 +0330

    Add message routers

---
+++ routers/message.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+
+from db import db_message
+from db.database import get_db
+from typing import List
+from .schemas import MessageBase, MessageDisplay, UserAuth
+from auth.oauth2 import get_current_user
+
+router = APIRouter(
+    prefix='/message',
+    tags=['message']
+)
+
+# Create Message
+@router.post('', response_model=MessageDisplay)
+def create_message(request: MessageBase, db: Session=Depends(get_db), current_user: UserAuth=Depends(get_current_user)):
+    return db_message.create_message(db, request, current_user)
+
+# Read all Messages
+@router.get('/all', response_model=List[MessageDisplay])
+def get_all_messages(db: Session=Depends(get_db), current_user: UserAuth=Depends(get_current_user)):
+    return db_message.get_all_messages(db)
+
+# Read one Message
+@router.get('/{id}')
+def get_message(id: int, db: Session=Depends(get_db), current_user: UserAuth=Depends(get_current_user)):
+    return db_message.get_message(db, id)
+
+# Update Message
+@router.put('/update/{id}')
+def update_message(id: int, request: MessageBase, db: Session=Depends(get_db), current_user: UserAuth=Depends(get_current_user)):
+    print('*'*50, current_user)
+    return db_message.update_message(db, id, request, current_user)
+
+# Delete Message
+@router.delete('/delete/{id}')
+def delete_message(id: int, db: Session=Depends(get_db), current_user: UserAuth=Depends(get_current_user)):
+    return db_message.delete_message(db, id, current_user)
